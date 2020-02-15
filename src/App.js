16ffd6719cd89862ba898b46287ed4d85c88a6cc
dahabu saidi <dahabusaidi@gmail.com>
2020-02-15 17:08:33 +0300

    Load data from props

--- src/App.js
+++ src/App.js
@@ -4,9 +4,27 @@ import './index.css'
 
 class App extends Component {
   render() {
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
     return (
       <div className="container">
-        <Table/>
+        <Table characterData={characters}></Table>
       </div>
     )
   }
