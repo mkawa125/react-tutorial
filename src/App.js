96a35f462f24ca36a8336a257c454ae6749cd37f
dahabu saidi <dahabusaidi@gmail.com>
2020-02-16 15:19:13 +0300

    Add form submit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React, { Component } from 'react';
-import Table from './components/Table'
+import Table from './components/Table';
+import Form  from "./components/Form";
 import './index.css'
 
 class App extends Component
 {
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ],
+    characters: [],
   }
   removeCharacter = index => {
   const { characters } = this.state
@@ -32,12 +16,16 @@ class App extends Component
       return i !== index
     }),
   })
+  }
+  handleSubmit = character => {
+  this.setState({ characters: [...this.state.characters, character] })
 }
   render() {
     const { characters } = this.state
     return (
       <div className="container">
-        <Table characterData={characters} removeCharacter={this.removeCharacter}></Table>
+        <Table characterData={ characters } removeCharacter={ this.removeCharacter }></Table>
+        <Form handleSubmit={this.handleSubmit} ></Form>
       </div>
     )
   }
