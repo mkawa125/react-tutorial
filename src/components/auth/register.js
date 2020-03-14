f282b683d005ebf28d32b832f2d5c72f4efa57a3
dahabu saidi <dahabusaidi@gmail.com>
2020-03-14 14:06:17 +0300

    Keep login and register page centered

--- src/components/auth/register.js
+++ src/components/auth/register.js
@@ -29,27 +29,29 @@ class Register extends Component {
 render() {
   const { name, job } = this.state;
     
-  return (
-    <form>
-      <label for="name">Name</label>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        className="form-control"
-        value={name}
-        onChange={this.handleChange} />
-      <label for="job">Job</label>
-      <input
-        type="text"
-        name="job"
-        id="job"
-        className="form-control"
-        value={job}
-        onChange={ this.handleChange } />
-          
-          <button className="btn btn-sm btn-success" type="button" value="Submit" onClick={this.submitForm}> Register</button>
-    </form>
+    return (
+      <div className='col-md-4 offset-4'>
+            <form>
+            <label for="name">Name</label>
+            <input
+                type="text"
+                name="name"
+                id="name"
+                className="form-control"
+                value={name}
+                onChange={this.handleChange} />
+            <label for="job">Job</label>
+            <input
+                type="text"
+                name="job"
+                id="job"
+                className="form-control"
+                value={job}
+                onChange={ this.handleChange } />
+                
+                <button className="btn btn-sm btn-success" type="button" value="Submit" onClick={this.submitForm}> Register</button>
+                </form>
+        </div>
   );
 }
 }
