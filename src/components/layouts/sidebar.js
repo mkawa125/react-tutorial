089440910ff3971912692bb8ca26eefebf20ecac
dahabu saidi <dahabusaidi@gmail.com>
2020-04-04 18:26:03 +0300

    Change logo

--- src/components/layouts/sidebar.js
+++ src/components/layouts/sidebar.js
@@ -22,7 +22,7 @@ render() {
               
               <li className="">
                <Link to="/">
-                  <i className="fa fa-users"></i> <span>Users</span>
+                  <i className="fa fa-users"></i> <span>Users Management</span>
                   <span className="pull-right-container">
                   </span>
                </Link>
