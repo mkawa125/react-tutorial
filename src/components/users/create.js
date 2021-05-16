9e067ed51b78c571fdab4172ee1884f28b40d942
Dahabu Saidi <dahabusaidi@gmail.com>
2021-05-16 18:45:24 +0300

    Change register page

--- src/components/users/create.js
+++ src/components/users/create.js
@@ -69,116 +69,166 @@ render() {
             </h1>
             <span className="">
                 <span> <Link to="/">Home</Link></span>
-                <span className="active"> / Users</span>
+                <span> <Link to="/users"> / Users</Link></span>
+                <span className="active"> / Create New User</span>
             </span>
-
-            <button className="btn btn-default add-user float-right"> <Link to="/users/add-new-user">Add New User</Link></button>
         </section>
     
 
-    <section className="content">
-      <div className="row">
-        <div className="col-md-12">
-              <div className="">
-                <div className="col-md-offset-2 col-md-6 col-md-offset-2">
-                          {error !== undefined && <div className={name} role="alert">{msg}</div>}
-                      </div>
-
-                <div className='col-md-6 col-md-offset-3'>
-                  <div className="col-md-12 login-box">
-                    
-                <div className="login-head" >Create Account</div>
-
-                    <div className="form-body">
-                      
-                <form  className="form-horizontal" method="POST" onSubmit= {this.onSubmit.bind(this)}> 
-                    <div className="form-group">
-                        <label className="lables" htmlFor="name">Full Names</label>
-                        <input
-                            type="text"
-                            name="name"
-                            id="name"
-                            ref="name"
-                            placeholder="Full names e.g Dahabu Mkawa"
-                            className="form-control"
-                            onChange={this.onChange.bind(this)} required autoFocus  />
-              </div>
-              
-
-              <div className="form-group">
-                        <label className="lables"  htmlFor="email">Email Address</label>
-                        <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            ref="email"
-                            placeholder="Email address e.g dahabusaidi@gmail.com"
-                            className="form-control"
-                            onChange={this.onChange.bind(this)} required />
+        <section className="content">
+        <div className="row">
+            <div className="col-md-12">
+                <div className="">
+                    <div className="col-md-offset-2 col-md-6 col-md-offset-2">
+                        {error !== undefined && <div className={name} role="alert">{msg}</div>}
                     </div>
 
-              <div className="form-group">
-                        <label className="lables"  htmlFor="username">Username</label>
-                        <input
-                            type="text"
-                            name="username"
-                            id="username"
-                            ref="username"
-                            placeholder="Username e.g mkawa125"
-                            className="form-control"
-                            onChange={this.onChange.bind(this)} required />
-                    </div>
-
-                    <div className="form-group">
-                        <label className="lables" htmlFor="password">Password</label>
-                            
-                        <div>
-                            <input
-                            type="password"
-                            name="password"
-                            placeholder="Password"
-                              id="password"
-                              ref="password"
-                            className="form-control"
-                            onChange={this.onChange.bind(this)} required />
-                        </div>
+                    <div className='col-md-8 col-md-offset-2 main-content'>
+                        <div className="col-md-12">
                         
-              </div>
-              
-              <div className="form-group">
-                        <label className="lables" htmlFor="password_confirmation">Confirm Password</label>
+                        <div className="login-head" >Create Account</div>
+
+                        <div className="form-body">
+                            <form  className="form-horizontal" method="POST" onSubmit= {this.onSubmit.bind(this)}>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <div className="form-group">
+                                            <div className="col-md-12">
+                                                <label className="lables" htmlFor="name">Full Names</label>
+                                                <input
+                                                    type="text"
+                                                    name="name"
+                                                    id="name"
+                                                    ref="name"
+                                                    placeholder="Full Names"
+                                                    className="form-control"
+                                                    onChange={this.onChange.bind(this)} required autoFocus/>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-md-6">
+                                        <div className="form-group">
+                                            <div className="col-md-12">
+                                                <label className="lables"  htmlFor="email">Email Address</label>
+                                                <input
+                                                    type="email"
+                                                    name="email"
+                                                    id="email"
+                                                    ref="email"
+                                                    placeholder="Email Address"
+                                                    className="form-control"
+                                                    onChange={this.onChange.bind(this)} required />
+                                            </div>
+                                        </div>
+                                    </div> 
+                                </div>
+
+                                <div className="row">
+                                    <di className="col-md-6">
+                                        <div className="form-group">
+                                            <div className="col-md-12">
+                                                <label className="lables"  htmlFor="username">Username</label>
+                                                <input
+                                                    type="text"
+                                                    name="username"
+                                                    id="username"
+                                                    ref="username"
+                                                    placeholder="Username e.g mkawa125"
+                                                    className="form-control"
+                                                    onChange={this.onChange.bind(this)} required />
+                                            </div>
+                                        </div>
+                                    </di>
+
+                                    <di className="col-md-6">
+                                        <div className="form-group">
+                                            <div className="col-md-12">
+                                                <label className="lables"  htmlFor="phoneNumber">Phone Number</label>
+                                                <input
+                                                    type="text"
+                                                    name="phoneNumber"
+                                                    id="phoneNumber"
+                                                    ref="phoneNumber"
+                                                    placeholder="+255 717495198"
+                                                    className="form-control"
+                                                    onChange={this.onChange.bind(this)} required />
+                                            </div>
+                                        </div>
+                                    </di>
+                                </div>
+
+                                <div className="row">
+                                    <di className="col-md-6">
+                                        <div className="form-group">
+                                            <div className="col-md-12">
+                                                <label className="lables"  htmlFor="username">Role</label>
+                                                <select
+                                                name="role"
+                                                id="role"
+                                                onChange={this.onChange.bind(this)} required
+                                                className="form-control">
+                                                    <option value="Admin">Admin</option>
+                                                    <option value="User">User</option>
+                                                    <option value="Manager">Manager</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </di>
+
+                                    <di className="col-md-6">
+                                        <div className="form-group">
+                                            <div className="col-md-12">
+                                            <label className="lables" htmlFor="password">Password</label>   
+                                                <input
+                                                    type="password"
+                                                    name="password"
+                                                    placeholder="Password"
+                                                    id="password"
+                                                    ref="password"
+                                                    className="form-control"
+                                                    onChange={this.onChange.bind(this)} required />
+                                            </div>
+                                        </div>
+                                    </di>
+                                </div>
                             
-                        <div>
-                            <input
-                            type="password"
-                            name="password_confirmation"
-                            placeholder="Confirm Password"
-                            id="password_confirmation"
-                            ref="confirm"
-                            className="form-control"
-                            onChange={this.onChange.bind(this)} required />
-                        </div>
-                        
-                    </div>
+                                {/* <div className="form-group">
+                                    <label className="lables" htmlFor="password_confirmation">Confirm Password</label>   
+                                    <div>
+                                        <input
+                                        type="password"
+                                        name="password_confirmation"
+                                        placeholder="Confirm Password"
+                                        id="password_confirmation"
+                                        ref="confirm"
+                                        className="form-control"
+                                        onChange={this.onChange.bind(this)} required />
+                                    </div>
+                                </div> */}
 
-                    <div className="row">
-                        <button className="btn btn-sm btn-block login-button" type="submit"> Register</button>
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        <button className="btn btn-sm login-button" type="submit"> 
+                                            Register
+                                        </button>
+                                    </div>
+                                </div>
+                        </form>
+                    </div>
+                
+                    {/* <div className="not-registered">
+                        Already have an accunt ? <Link to="/login">login</Link>
+                    </div> */}
 
                     </div>
-                    </form>
+                
+                    </div>
+                </div>
             </div>
-            
-            <div className="not-registered">
-          Already have an accunt ? <Link to="/login">login</Link>
-        </div>
-        </div>
-        
-        </div>
-              </div>
-        </div>
-      </div>      
-    </section>
-</div>
+        </div>      
+        </section>
+    </div>
   );
 }
 }
